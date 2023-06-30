348c4b6ce079c3b0312e0506213f6d380f35100a
trung257 <trungtoru25@gmail.com>
2023-06-30 10:08:31 +0700

    Update LoginScreen

--- Component/LoginScreen.js
+++ Component/LoginScreen.js
@@ -1,40 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { useNavigation, HeaderBackButton } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
+  const [emailOrPhoneNumber, setEmailOrPhoneNumber] = useState(''); 
+  const [password, setPassword] = useState('');
 
-  const getData = async () => {
+  const handleLogin = async () => {
     try {
-      const value = await AsyncStorage.getItem('my-key');
+      const value = await AsyncStorage.getItem('user');
       if (value !== null) {
-        // value previously stored
+        const user = JSON.parse(value);
+        if ((user.email === emailOrPhoneNumber || user.phoneNumber === emailOrPhoneNumber) && user.password === password) {
+          Alert.alert('Đăng nhập thành công!');
+          navigation.navigate('Home');
+        } else {
+          console.log('Thông tin đăng nhập không đúng');
+          Alert.alert('Thông tin đăng nhập không đúng!');
+        }
       }
     } catch (e) {
-      // error reading value
+      console.error(e);
     }
   };
 
-  const handleLogin = () => {
-    // Xử lý đăng nhập
-    navigation.navigate('Home');
+  const handleFacebookLogin = async () => {
+    try {
+      const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+      if (result.isCancelled) {
+        console.log('Đăng nhập bằng Facebook bị hủy bỏ');
+      } else {
+        const data = await AccessToken.getCurrentAccessToken();
+        if (!data) {
+          console.log('Không lấy được token từ Facebook');
+        } else {
+          const response = await fetch(`https://graph.facebook.com/me?access_token=${data.accessToken}`);
+          const userData = await response.json();
+          await AsyncStorage.setItem('user', JSON.stringify(userData));
+          navigation.navigate('Home');
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
   };
 
-  const handleFacebookLogin = () => {
-    // Xử lý đăng nhập bằng Facebook
-    navigation.navigate('Home');
+  const handleEmailOrPhoneNumberChange = (text) => {
+    if (/^\d+$/.test(text)) {
+      // Nếu text chỉ chứa các số, đây có thể là số điện thoại
+      setEmailOrPhoneNumber(text);
+    } else {
+      // Nếu text không chỉ chứa các số, đây có thể là email
+      setEmailOrPhoneNumber(text.toLowerCase());
+    }
   };
 
   return (
     <View style={styles.container}>
       <HeaderBackButton onPress={() => navigation.navigate('Landing')} />
       <Text style={styles.title}>Sign in</Text>
-      <TextInput style={styles.input} placeholder="Email or phone number" />
-      <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} />
+      <TextInput
+        style={styles.input}
+        placeholder="Email or phone number"
+        value={emailOrPhoneNumber}
+        onChangeText={handleEmailOrPhoneNumberChange}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
       <Button title="Log in" onPress={handleLogin} style={styles.button} />
-      <Button title="Facebook login" onPress={handleFacebookLogin} style={styles.facebookButton} />
+      <Button
+        title="Facebook login"
+        onPress={handleFacebookLogin}
+        style={styles.facebookButton}
+      />
     </View>
   );
 };
